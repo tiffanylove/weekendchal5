6e6001677197e27598c9ea94719d39ee849e60d7
Tiffany Love <tnlove_99@yahoo.com>
2017-05-15 15:36:11 -0500

    updated html

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,18 +1,27 @@
 var myApp = angular.module('myApp',[]);
 
-myApp.controller('MovieController', function($http, MovieTracker){
+myApp.controller('MovieController', function($http){
   console.log('controller');
 
    var vm = this;
    vm.test = 'Hello';
    vm.movies = [];
 
-   vm.getMovies = function(){
-     MovieTracker.getMovies().then(function(data){
-       vm.movies = data;
+
+
+
+
+
+
+   vm.requestMovie = function(){
+    //  MovieTracker.getMovies().then(function(data){
+    $http({
+      method: 'GET',
+      url:'http://www.omdbapi.com/?s=<user search string>'
+    }).then(function success(response){
+      console.log('omdb response', response.data);
+       vm.movieData = response.data;
+       vm.movieData.push(vm.movies);
      });
    };
-
-
-
 });
